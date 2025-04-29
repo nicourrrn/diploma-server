707e2946e1ca38c06b733978d268b36add6d8992
nicourrrn <s57111702@gmail.com>
2025-04-29 13:51:02 +0300

    make base functional for auth system and work with db

--- python/pkg/utils.py
+++ python/pkg/utils.py
@@ -4,12 +4,12 @@ from typing import Optional
 
 import dotenv
 import jwt
-from fastapi import Depends, HTTPException, status
+from fastapi import HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 
 dotenv.load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "aslkdfjalskdfj")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES") or 60
 
